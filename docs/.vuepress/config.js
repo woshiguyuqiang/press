0e368becc8a3d15b5df27b963c843f6587cb6e30
gyq <13284050502@163.com>
2020-09-15 22:33:14 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: 'Adroi媒体API 接口文档',  // 设置网站标题
+    description : 'Adroi',
+    base : '/',
+    themeConfig : {
+      nav : [
+          { text: '接口定义', link: '/apiword' },
+          { text: '接口字段定义', link: '/api' },
+          { text: '附录：错误码', link: '/error' }
+      ],
+      sidebar: [
+          ['/', '简介'],
+          ['/apiword', '根目录创建 apiword.md文件'],
+          ['/api', '根目录创建 api.md文件'],
+          ['/error', '根目录创建 error.md文件']
+      ],
+      sidebarDepth : 2
+    }
+}
